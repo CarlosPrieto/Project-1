f339af1b2df6caf1ebdd6e3cae6f3d3890a4ffd5
Carlos Prieto <carlos2000.prieto@gmail.com>
2019-10-30 10:39:22 +0100

    Add coments to Data_Plot.py

--- Data_Plot.py
+++ Data_Plot.py
@@ -8,13 +8,17 @@ def dataPlot(data): #Recieves a N x 3 matrix with Temperature, Growth rate and B
     bac3 = data[data[:,2] == 3]
     bac4 = data[data[:,2] == 4]
 
+    #Plots the size of each of the arrays that were just defined with a bar chart
     plt.bar(np.arange(1,5),np.array([np.size(bac1,axis=0),np.size(bac2,axis=0),np.size(bac3,axis=0),np.size(bac4,axis=0)]), width=0.75)
     plt.title("Number of Bacteria")
     plt.xlabel("Type of Bacteria")
     plt.ylabel("Frequency")
+    #Change the x-axis ticks from numbers to the name of each bacteria
     plt.xticks(ticks=[1,2,3,4],labels=["Salmonella","Bacillus cereus","Listeria","Brochothrix"])
     plt.show()
 
+    #Plots Growth rate against temperature for each type of bacteria individually
+    #Gives each line a color in RGB from 0 to 1 and adds a dot at each data point
     plt.plot(bac1[:,0],bac1[:,1], color=((0,0.25,0.9)), marker="o")
     plt.plot(bac2[:,0],bac2[:,1], color=((1,0.535,0.1)), marker="o")
     plt.plot(bac3[:,0],bac3[:,1], color=((0.0275,0.675,0.2)), marker="o")
